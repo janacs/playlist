4240910673a73237dd5b8cafab6953d3ac613b40
Janderson Sousa <jandersonsouza_phn@hotmail.com>
2024-10-30 10:57:04 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Card from "./Components/Card";
+
+export default function App() {
+  return (
+    <>
+      <h1>Playlist</h1>
+      <Card artista="Rainbow" />
+      <Card artista="Fleetwood Mac" />
+      <Card artista="TOTO" />
+      <Card artista="Dire Straits" />
+      <Card artista="Journey" />
+    </>
+  );
+}
---
+++ src/Components/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card({ artista, musica, dia, imagem }) {
+  return (
+    <section>
+      <h2>{artista}</h2>
+    </section>
+  );
+}
