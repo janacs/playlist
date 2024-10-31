4df523f99be9dbfd691c898eea9a7cf555f2ca6f
Janderson Sousa <jandersonsouza_phn@hotmail.com>
2024-10-31 05:13:08 +0000

    bandasAtualizadas

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,23 @@
-export default function Card({ artista, musica, dia, imagem }) {
+import styled from "styled-components";
+
+const Section = styled.section`
+  border: 1px solid #ddd;
+  width: 300px;
+
+  img {
+    width: 100%;
+  }
+`;
+
+export default function Card({ artista, musica, foto, dia }) {
   return (
-    <section>
-      <h2>{artista}</h2>
-    </section>
+    <Section>
+      {/* <h2>Entreguem o desafio da Lampada</h2> */}
+      <h2>
+        {artista} - {musica}
+      </h2>
+      <img src={foto} alt={`Foto do artista ${artista}`} />
+      <p>{dia}</p>
+    </Section>
   );
 }
